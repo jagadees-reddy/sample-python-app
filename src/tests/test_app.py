fd13405a477ae50c9e3f1a9e907d64adb83b301f
jagadees-reddy <jagadeeswar.reddy3@wipro.com>
2024-09-13 15:57:19 +0100

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,1 +1,14 @@
+# tests/test_app.py
+
+import unittest
+from sample_python_app import app
+
+class TestApp(unittest.TestCase):
+
+    def test_sample(self):
+        result = app.sample_function()
+        self.assertEqual(result, 'Hello, World!')
+
+if __name__ == '__main__':
+    unittest.main()
 
